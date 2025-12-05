6a6d60a87fa71562714ccf74e49433aeffdc9f0b
melindaa30 <amelindasyafitrimelindaa@gmail.com>
2025-12-05 11:34:20 +0700

    Add files via upload

---
+++ biodatadiri.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biodata Diri</title>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+		}
+		
+		.biodata {
+			width: 50%;
+			margin: 40px auto;
+			padding: 20px;
+			border: 1px solid #ddd;
+			border-radius: 10px;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+		}
+		
+		.biodata h2 {
+			text-align: center;
+			margin-bottom: 20px;
+		}
+		
+		.biodata p {
+			margin-bottom: 10px;
+		}
+		
+		.biodata span {
+			font-weight: bold;
+		}
+	</style>
+</head>
+<body>
+	<div class="biodata">
+		<h2>Biodata Diri</h2>
+		<p><span>Nama</span>: Belva sasya novita</p>
+		<p><span>Tempat, Tanggal Lahir</span>: GUMULUNG LEBAK, 15 NOVEMBER 2007
+</p>
+		<p><span>Jenis Kelamin</span>: 
+Perempuan</p>
+		<p><span>Alamat</span>: gumulung lebak</p>
+		<p><span>Pendidikan</span>: Pelajar</p>
+		<p><span>Hobi</span>: nyanyi</p>
+	</div>
+</body>
+</html>
